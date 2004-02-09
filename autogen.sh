1eb8019d9337b9d796a9d6d0fd6d5b5182a30d11
Sergey Poznyakoff <gray@gnu.org.ua>
2004-02-09 14:55:08 +0000

    Updated
    git-svn-id: file:///svnroot/pam-modules/trunk@7 56984be4-0537-0410-a56c-fcb268c96130

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
+if [ ! -d scripts ]; then
+	mkdir scripts
+fi
 aclocal &&
  libtoolize --automake -c &&
  autoheader &&
  automake -a -c &&
  autoconf
 
-if [ ! -d scripts ]; then
-	mkdir scripts
-fi
