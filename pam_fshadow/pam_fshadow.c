bd863c9bbdc7e73ef6020558519da032df38d59f
Sergey Poznyakoff <gray@gnu.org.ua>
2006-09-05 21:34:52 +0000

    Include <common.c>
    git-svn-id: file:///svnroot/pam-modules/trunk@30 56984be4-0537-0410-a56c-fcb268c96130

--- pam_fshadow/pam_fshadow.c
+++ pam_fshadow/pam_fshadow.c
@@ -38,7 +38,8 @@
 #define PAM_SM_ACCOUNT
 
 #include <security/pam_modules.h>
-#define XSTRDUP(s) (s) ? strdup(s) : NULL
+
+#include <common.c>
 
 /* some syslogging */
 
@@ -99,37 +100,6 @@ _pam_parse(int argc, const char **argv)
 	}
 	
 	return cntl_flags;
-}
-
-#define PAM_OVERWRITE(s)        \
-  do {                           \
-	register char *p;        \
-        if  ((p = s) != NULL)    \
-	    while (*p) *p++ = 0; \
-  } while (0) 
-
-#define PAM_DROP_REPLY(reply, nrepl)                 \
-  do {                                                \
-	int i;                                        \
-	for (i=0; i<nrepl; i++) {                     \
-            PAM_OVERWRITE(reply[i].resp);             \
-            free(reply[i].resp);                      \
-	}                                             \
-	if (reply)                                    \
-	    free(reply);                              \
-  } while (0)
-
-static void
-_pam_delete(char *x)
-{
-	PAM_OVERWRITE(x);
-	free(x);
-}
-
-static void
-_cleanup_string(pam_handle_t *pamh, void *x, int error_status)
-{
-	_pam_delete(x);
 }
 
 static int
@@ -428,7 +398,7 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags,
 /* static module data */
 
 struct pam_module _pam_fshadow_modstruct = {
-     "pam_userdb",
+     "pam_fshadow",
      pam_sm_authenticate,
      pam_sm_setcred,
      NULL,
--- pam_regex/pam_regex.c
+++ pam_regex/pam_regex.c
@@ -42,46 +42,7 @@
 #endif				/* LINUX_PAM */
 #include <security/pam_modules.h>
 
-#ifndef PAM_CONV_AGAIN
-# define PAM_CONV_AGAIN PAM_TRY_AGAIN
-#endif
-#ifndef PAM_AUTHTOK_RECOVER_ERR
-# define PAM_AUTHTOK_RECOVER_ERR PAM_AUTHTOK_RECOVERY_ERR
-#endif
-#ifndef PAM_EXTERN
-# define PAM_EXTERN
-#endif
-
-#define PAM_OVERWRITE(s)        \
-  do {                           \
-	register char *p;        \
-        if  ((p = s) != NULL)    \
-	    while (*p) *p++ = 0; \
-  } while (0) 
-
-#define PAM_DROP_REPLY(reply, nrepl)                 \
-  do {                                                \
-	int i;                                        \
-	for (i=0; i<nrepl; i++) {                     \
-            PAM_OVERWRITE(reply[i].resp);             \
-            free(reply[i].resp);                      \
-	}                                             \
-	if (reply)                                    \
-	    free(reply);                              \
-  } while (0)
-	
-static void
-_pam_delete(char *x)
-{
-	PAM_OVERWRITE(x);
-	free(x);
-}
-
-static void
-_cleanup_string(pam_handle_t *pamh, void *x, int error_status)
-{
-	_pam_delete(x);
-}
+#include <common.c>
 
 /* logging */
 static void
@@ -304,7 +265,7 @@ pam_sm_setcred(pam_handle_t *pamh,
 #ifdef PAM_STATIC
 
 struct pam_module _pam_radius_modstruct = {
-	"pam_radius",                      /* name of the module */
+	"pam_regex",                      /* name of the module */
 	pam_sm_authenticate,                 
 	pam_sm_setcred,
 	NULL,
